a3890cdbf92d94444f1763bb4c3f966c46f87f67
ding <ding@diinngg.com>
2020-01-21 12:51:01 +0000

    training stuff
    use telnet to communicate between script and rtt
    
    fix trying to learn from move 8
    
    print W/L before learning
    
    increase stack size to max available

--- train.py
+++ train.py
@@ -2,6 +2,23 @@
 
 
 import random
+import sys
+import telnetlib
+import time
+
+from collections import Counter
+from matplotlib import pyplot as plt
+
+
+port = 19021
+
+move_first = False
+
+max_games = 1e6
+
+game_delay = 0
+
+print_interval = 100
 
 
 def pick_box(board, skill_level):
@@ -10,32 +27,79 @@ def pick_box(board, skill_level):
     elif skill_level == 2:
         pass
     else:
-        return random.choice([i for i, x in enumerate(board) if x == 0])
+        try:
+            return random.choice([i for i, x in enumerate(board) if x == 0])
+        except IndexError:
+            return -1
 
 
 if __name__ == '__main__':
-    listen_chars = '012345678DWLE'
+    listen_chars = '012345678WDLE'
     record = []
+    record_stats = {c: [] for c in 'WDLE'}
 
     try:
-        while True:
-            print('F', end='')
+        t = telnetlib.Telnet(host='localhost', port=port)
+        print(t.read_until(b'???', 1).decode('ascii'))
+        t.write(b'R')
+
+        for i in range(int(max_games)):
+            t.write(b'F')
             board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-            while True:
-                c = input()[0]
-
-                if c not in listen_chars:
-                    continue
-                elif c in listen_chars[0:9]:
-                    board[int(c)] = 2
-                elif c in listen_chars[9:]:
-                    record.append(c)
-                    break
-
+            if move_first:
                 box = pick_box(board, 0)
                 board[box] = 1
-                print(str(box), end='')
+                t.write(str(box).encode('ascii'))
+            while True:
+                c = t.read_until(b'\n', 1).decode('ascii')
+                if not c:
+                    print(t.read_eager().decode('ascii'))
+                    raise Exception
+                if c[0] not in listen_chars:
+                    continue
+                elif c[0] in listen_chars[0:9]:
+                    board[int(c[0])] = 2
+                elif c[0] in listen_chars[9:]:
+                    record.append(c[0])
+                    break
+                else:
+                    raise Exception
+
+                box = pick_box(board, 0)
+                if box == -1:
+                    continue
+                board[box] = 1
+                t.write(str(box).encode('ascii'))
+            if not (len(record) % print_interval):
+                count = Counter(record[-print_interval:])
+                win_percent = 100 * count['W'] / print_interval
+                draw_percent = 100 * count['D'] / print_interval
+                loss_percent = 100 * count['L'] / print_interval
+                error_percent = 100 * count['E'] / print_interval
+                print(f'games: {i + 1:d}  '
+                      f'win: {win_percent:.2f}%  '
+                      f'draw: {draw_percent:.2f}%  '
+                      f'loss: {loss_percent:.2f}%  '
+                      f'error: {error_percent:.2f}%  '
+                      f'\r', file=sys.stderr, end='')
+                record_stats['W'].append(win_percent)
+                record_stats['D'].append(draw_percent)
+                record_stats['L'].append(loss_percent)
+                record_stats['E'].append(error_percent)
+            time.sleep(game_delay)
     except:
         raise
     finally:
-        print('R', end='')
+        t.write(b'R')
+        t.close()
+        x = [print_interval * i for i in range(len(record_stats['W']))]
+        plt.plot(x, record_stats['W'], 'g', label='win %')
+        plt.plot(x, record_stats['D'], 'y', label='draw %')
+        plt.plot(x, record_stats['L'], 'r', label='loss %')
+        plt.plot(x, record_stats['E'], 'm', label='error %')
+        plt.legend()
+        plt.grid()
+        plt.xlabel('games')
+        plt.ylabel('%')
+        plt.ylim(0, 100)
+        plt.show()
